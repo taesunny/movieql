329250fdec10d7124f07d6068d9bdc7b88622e3e
Taesun Lee <superbsun@gmail.com>
2020-04-30 17:02:30 +0900

    add quering url with limit, rating

--- graphql/db.js
+++ graphql/db.js
@@ -1,7 +1,15 @@
 import fetch from "node-fetch";
-const API_URL = "https://yts.mx/api/v2/list_movies.json"; //?limit=50&minimum_rating=9"
+const API_URL = "https://yts.mx/api/v2/list_movies.json?"; //?limit=50&minimum_rating=9"
 
-export const getMovies = (limit, rating) =>
-  fetch(`${API_URL}`)
+export const getMovies = (limit, rating) => {
+  let REQUEST_URL = API_URL;
+  if (limit > 0) {
+    REQUEST_URL += `limit=${limit}`;
+  }
+  if (rating > 0) {
+    REQUEST_URL += `&minimum_rating=${rating}`;
+  }
+  return fetch(REQUEST_URL)
     .then((res) => res.json())
     .then((json) => json.data.movies);
+};
